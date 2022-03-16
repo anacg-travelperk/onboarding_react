cdff76e0065892f033a0c9e40b63ab1b1d50c909
Ana <ana.chacon@travelperk.com>
2022-03-16 15:57:02 +0100

    Create CreateRecipe component

---
+++ src/CreateRecipe.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { useState } from "react";
+const axios = require("axios")
+
+const submitRecipeInfo = async (recipe, ingredientList) => {
+    const ingredients = ingredientList.map(ingr => ({"name": ingr}))
+    const res = axios.post('http://localhost:8000/recipes/', {
+        name: recipe.name,
+        description: recipe.description,
+        ingredients: ingredients
+    })
+    return res
+}
+
+const CreateRecipe = () => {
+    const [recipe, setRecipe] = useState({
+        name: '',
+        description: '',
+    });
+    const [ingredient, setIngredient] = useState("")
+    const [ingredientList, setIngredientList] = useState([])
+
+
+    const handleRecipeChange = (e) => {
+        console.log(e)
+        const { name, value } = e.target
+        setRecipe((recipe) => ({
+            ...recipe,
+            [name]: value,
+        }))
+    }
+
+    const handleIngredientChange = (e) => {
+        const newIngredient = e.target.value
+        setIngredient(newIngredient)
+    }
+
+    const addIngredientToList = () => {
+        if (ingredient != ("")) {
+            setIngredientList(() => ([
+                ...ingredientList,
+                ingredient,
+            ]))
+            setIngredient("")
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const response = await submitRecipeInfo(recipe, ingredientList)
+        if (response.status === 201) {
+            setIngredient("")
+            setIngredientList([])
+            setRecipe({name: '',
+            description: '',})
+        }
+    }
+
+    return(
+        <div>
+            <h1>New recipe:</h1>
+            <form onSubmit={handleSubmit}>
+                <label for="name">Recipe name: </label>
+                <input name="name" type= "text" value={recipe.name} onChange={handleRecipeChange}/>
+                <br/>
+                <label for="description">Recipe description: </label>
+                <input name="description" type= "text" value={recipe.description} onChange={handleRecipeChange}/>
+                <br/>
+                <label for="ingredient">Ingredient: </label>
+                <input name="ingredient" type= "text" value={ingredient} onChange={handleIngredientChange}/>
+                <button title="add one more ingredient" onClick={addIngredientToList} type="button">+</button> 
+                <ul>
+                    {ingredientList.map((value, index) => {
+                        return <li key={index}>{value}</li>
+                    })}
+                </ul>
+                <button type="submit">Create</button>
+            </form>
+        </div>
+    )
+}
+
+export default CreateRecipe
